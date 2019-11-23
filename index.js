9d483b40c6ecd4c83fdec286ae547b1e8cc81423
Yunus Gaziev <yunusga@yandex.ru>
2019-11-23 21:33:09 +0500

    Add sorting functionality

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 let postcss = require('postcss')
+const sortCSSmq = require('sort-css-media-queries')
 
 module.exports = postcss.plugin('postcss-sort-media-queries', (opts = { }) => {
+  opts = Object.assign({
+    sort: 'mobile-first'
+  }, opts)
 
-  // Work with options here
+  return root => {
+    let atRules = {}
 
-  return (root, result) => {
+    function sortAtRules (queries, sort) {
+      if (typeof sort === 'function') {
+        return queries.sort(sort)
+      }
 
-    // Transform CSS AST here
+      if (typeof sort === 'string') {
+        sort = (sort === 'desktop-first') ? sortCSSmq.desktopFirst : sortCSSmq
+        return queries.sort(sort)
+      }
 
+      return queries
+    }
+
+    root.walkAtRules('media', atRule => {
+      let query = atRule.params
+
+      if (!atRules[query]) {
+        atRules[query] = postcss.atRule({
+          name: atRule.name,
+          params: atRule.params
+        })
+      }
+
+      atRule.nodes.forEach(node => {
+        atRules[query].append(node.clone())
+      })
+
+      atRule.remove()
+    })
+
+    sortAtRules(Object.keys(atRules), opts.sort).forEach(query => {
+      root.append(atRules[query])
+    })
   }
 })
