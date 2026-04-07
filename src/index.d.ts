85151f26997cdc93e09577736930cec343f0e102
Yunus Gaziev <yunusga@yandex.ru>
2026-04-07 06:42:55 +0500

    feat: Add types to module
    Closes #73 Request support for TypeScript

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+import { Plugin } from 'postcss';
+
+type SortOption = 'mobile-first' | 'desktop-first' | ((a: string, b: string) => number);
+
+interface Options {
+  sort?: SortOption;
+  configuration?: {
+    unitlessMqAlwaysFirst?: boolean;
+  };
+}
+
+declare const plugin: (options?: Options) => Plugin;
+
+export = plugin;
